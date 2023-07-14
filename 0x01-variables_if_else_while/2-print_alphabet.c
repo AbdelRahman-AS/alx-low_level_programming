6415500c91bf92dbc7c6ccb425ee63f4a967c06c
AbdelRahman Hanafy <abdelrahmansam@gmail.com>
2023-07-14 05:01:13 -0700

    io

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#!/bin/bash
+git add .
+git commit -m 'io'
+git push origin master
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
+}
