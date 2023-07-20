79939609cfa082cc071158156e2384cd0f22c597
AbdelRahman-AS <121566682+AbdelRahman-AS@users.noreply.github.com>
2023-07-20 13:22:36 +0000

    a

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -2,7 +2,7 @@
 
 /**
  * print_diagonal - run numbers
- *
+ *@n: int
  * Return: Always 0.
  */
 void print_diagonal(int n)
@@ -10,8 +10,8 @@ void print_diagonal(int n)
 int i;
 int x;
 for (x = 0 ; x < n; x++)
-{    
-for (i = 0 ; i < n; i++)
+{
+for (i = 0 ; i < x; i++)
 {
 _putchar(' ');
 }
