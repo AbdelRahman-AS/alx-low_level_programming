bf59885184cfd4453bfc83fb097b4f414574753c
AbdelRahman-AS <121566682+AbdelRahman-AS@users.noreply.github.com>
2023-07-20 11:06:30 +0000

    a

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,16 +1,20 @@
 #include "main.h"
 
 /**
- * print_numbers - check the code
+ * print_most_numbers - check the code
  *
  * Return: Always 0.
  */
-void print_numbers(void)
+void print_most_numbers(void)
 {
 int i;
 for (i = 0 ; i < 10; i++)
 {
+
+if (i != 2 && i != 4)
+{
 _putchar(i + '0');
+}
 /*
 *char i;
 *for (i = 48 ; i < 58; i++)
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -5,6 +5,9 @@
 int _isupper(char c);
 int _isdigit(char c);
 int mul(int a, int b);
-void print_numbers();
+void print_numbers(void);
+int _putchar(char);
+void print_most_numbers(void);
+
 
 #endif /* MAIN_H */
