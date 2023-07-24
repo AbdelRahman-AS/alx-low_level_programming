902844704446e8c73865eeea4187725d80078a32
AbdelRahman Hanafy <abdelrahmansam@gmail.com>
2023-07-24 08:43:17 -0700

    io

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *swap_int - swap int
+ *@a: dsfsf
+ *@b: dasd
+ * Return: Always 0.
+ */
+void swap_int(int *a, int *b)
+{
+int temp;
+temp = *a;
+*a = *b;
+*b = temp;
+}
