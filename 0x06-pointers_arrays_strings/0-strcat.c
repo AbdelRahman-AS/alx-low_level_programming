b9b473c1d56df72c8023462fa30edcca603aead9
AbdelRahman Hanafy <abdelrahmansam@gmail.com>
2023-07-26 07:38:51 -0700

    io

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcat - append strings
+ *@dest: fdvdvfdv
+ *@src: lfdgfgfg
+ * Return: Always 0.
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	while (*dest++ != '\0')
+	{
+		i++;
+	}
+
+	while (i != 
+	(src + i)
+
+
+
+}
