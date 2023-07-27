15300409cea035ee0464bca6af47e0bdd8a800ae
AbdelRahman Hanafy <abdelrahmansam@gmail.com>
2023-07-27 10:38:24 -0700

    io

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -13,10 +13,14 @@ int _strcmp(char *s1, char *s2)
 	int i;
 	int x;
 
-	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
-		if (s1 == s2)
+	for (i = 0; ; i++)
+		if (s1[i] == s2[i])
+		{
 			x = 0;
-		else if (s1 > s2)
+			if (s1[i] == '\0')
+				break;
+		}
+		else if (s1[i] > s2[i])
 		{
 			x = 15;
 			break;
