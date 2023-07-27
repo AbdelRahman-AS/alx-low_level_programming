15dd95bbf045a68babba14d75e4774c64d0ba968
AbdelRahman Hanafy <abdelrahmansam@gmail.com>
2023-07-27 10:59:03 -0700

    io

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ * @a: string
+ * @n: int
+ *
+ * Return: void
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i;
+	int x;
+
+	for (i = 0; ; i++)
+		if (s1[i] == s2[i])
+		{
+			x = 0;
+			if (s1[i] == '\0')
+				break;
+		}
+		else
+		{
+			x = s1[i] - s2[i];
+			break;
+		}
+}
+
