c7bd8100b07b72e996aac6d415cef3ec45b93869
AbdelRahman Hanafy <abdelrahmansam@gmail.com>
2023-07-27 10:50:04 -0700

    io

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,7 +5,7 @@
  * @s1: string 1
  * @s2: string 2
  *
- * Return: a pointer to the resulting string
+ * Return: int
  */
 
 int _strcmp(char *s1, char *s2)
@@ -20,17 +20,11 @@ int _strcmp(char *s1, char *s2)
 			if (s1[i] == '\0')
 				break;
 		}
-		else if (s1[i] > s2[i])
-		{
-			x = 15;
-			break;
-		}
 		else
 		{
-			x = -15;
+			x = s1[i] - s2[i];
 			break;
 		}
-
 	return (x);
 }
 
