c59d30929450ba484a8a85a7998b5c8b6e029c24
AbdelRahman Samy <asamy1994@live.com>
2023-08-10 21:54:56 +0300

    io

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+/**
+* malloc_checked - code
+*@b: int
+* Return: void.
+*/
+
+void *malloc_checked(unsigned int b)
+{
+    int *x;
+    x = malloc(sizeof(b));
+    if(x == NULL)
+    {
+        exit(98)
+    }
+}
