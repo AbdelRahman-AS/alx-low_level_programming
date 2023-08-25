baf8f5720c81982098a83de0ba7540b44b4d11bf
AbdelRahman Hanafy <abdelrahmansam@gmail.com>
2023-08-24 23:55:45 -0700

    io

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,11 +3,13 @@
 void first(void) __attribute__ ((constructor));
 
 /**
- * first - prints You're beat! and yet, you must allow,\nI bore my house upon my back!\n before the main function is executed.
+ * first - prints You're beat! and yet, you must allow,\n
+ * I bore my house upon my back!\n before the main function is executed.
  *
  */
 void first(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\nI bore my house upon ");
+	printf("back!\n");
 }
 
